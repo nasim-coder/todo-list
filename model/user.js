166b308865a6054492ed6c373ffc3f04c4284475
NakRex <khannasimahmad@gmail.com>
2022-03-02 21:48:04 +0530

    added validation in user model

--- model/user.js
+++ model/user.js
@@ -6,13 +6,29 @@ let userSchema = new mongoose.Schema({
         required: true
     },
     email: {
-        type: String,
-        rquired: true
+        type: mongoose.SchemaType.email,
+        unique: true,
+        rquired: 'correct email is required',
+        match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email address.',],
+        dropDups: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /d{10}/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
     },
     password: {
         type: String,
-        required: true
-    }
+        required: true,
+        minlength: [6, 'password shold be more than 6 character'],
+        maxlength:[15, 'password should be less than 15 character']
+    } 
 });
 
 let User = mongoose.model('User', userSchema);
