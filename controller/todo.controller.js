6181821f25f47565b0c16b46478247de044456ed
NakRex <khannasimahmad@gmail.com>
2022-03-06 21:09:38 +0530

    added register mehod

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,5 +1,17 @@
+const User = require('../model/user')
 exports.register = (req, res) => {
-    
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8)
+    });
+    user.save((err, user) => {
+        if (err) {
+            res.status('500').send({ message: err })
+            return
+        }
+        res.status(200).json('you are registered successfully')
+    });
 }
 
 exports.login = (req, res) => {
