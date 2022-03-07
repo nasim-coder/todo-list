1e02cbe4a607a9fee419af1e10f7dd42019b5b38
NakRex <khannasimahmad@gmail.com>
2022-03-07 21:37:15 +0530

    edited register function

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,17 +1,24 @@
 const User = require('../model/user')
 exports.register = (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8)
-    });
-    user.save((err, user) => {
-        if (err) {
-            res.status('500').send({ message: err })
-            return
-        }
-        res.status(200).json('you are registered successfully')
-    });
+    const { name, email,phone, password } = req.body;
+    let isAlreadyexist = await User.findOne({ email });
+    if (!isAlreadyexist) {
+        let user = new User({
+            name: name,
+            email: email,
+            phone:phone,
+            password: bcrypt.hashSync(password, 8)
+        });
+        user.save((err, user) => {
+            if (err) {
+                return res.status(500).send({ success: false, message: err })
+            
+            }
+            return res.status(200).json('you are registered successfully')
+        });
+    } else {
+        return res.status(501).send({msg: 'user already exist', success: false});
+    }
 }
 
 exports.login = (req, res) => {
