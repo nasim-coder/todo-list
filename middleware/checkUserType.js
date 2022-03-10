9a253977f72a05fc66412161f1edafb120221b51
NakRex <khannasimahmad@gmail.com>
2022-03-10 13:59:18 +0530

    added middleware to check admin

---
+++ middleware/checkUserType.js
@@ -0,0 +1,8 @@
+exports.checkUserType = (req, res, next)=>{
+    if (req.user.role == "admin") {
+        next()
+    }
+    else {
+        return res.status(403).send({success: false, msg:"only admin can access ths route"})
+    }
+}
