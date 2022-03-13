b7a380e76f318e01f9d8495c44db265b882af9a8
NakRex <khannasimahmad@gmail.com>
2022-03-13 10:32:13 +0530

    installed bcryptjs

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4000;
 const todoRouter = require('./route/todoRoute')
-mongoose.connect('mongodb://localhost:27017/todos', () => {
-    console.log(`mongodb connected`);
-})
+mongoose.connect('mongodb://localhost:27017/todos', () => console.log(`mongodb connected`))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use('/todo', todoRouter);
